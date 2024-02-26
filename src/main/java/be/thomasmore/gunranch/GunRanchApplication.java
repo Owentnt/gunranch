8ffe24ba1f07a7b14063d2b5a682f11ebcfc4ab1
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-02-26 18:05:30 +0100

    initial commit

---
+++ src/main/java/be/thomasmore/gunranch/GunRanchApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.gunranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunRanchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunRanchApplication.class, args);
+	}
+
+}
