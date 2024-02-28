f5cc866ee5e2ceb75a89cf9b6b02999e4ff04176
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-02-28 11:09:59 +0100

    problemen met controllers en database opgelost

--- src/main/java/be/thomasmore/gunranch/model/user.java
+++ src/main/java/be/thomasmore/gunranch/model/user.java
@@ -16,7 +16,54 @@ public class user {
 
     private String phoneNumber;
 
+    public user(int id, String firstName, String lastName, String emailAdres, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAdres = emailAdres;
+        this.phoneNumber = phoneNumber;
+    }
 
+    public user() {
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAdres() {
+        return emailAdres;
+    }
+
+    public void setEmailAdres(String emailAdres) {
+        this.emailAdres = emailAdres;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,48 @@
+INSERT INTO GUNS(ID, NAME, TYPE,MAGAZINE,CALIBER,PRICE,BIO)
+VALUES (1, 'Glock-19', 'Semi-Automatic-Pistol', 15, '9x19 mm', 8, 'Basic Gun');
+
+ INSERT INTO GUNS(ID, NAME, TYPE, MAGAZINE, CALIBER, PRICE,BIO)
+VALUES (2, 'Desert Eagle', 'Semi-Automatic-Pistol', 8, '.44 Magnum', 8, 'Basic Gun');
+
+    INSERT INTO GUNS(ID, NAME, TYPE, MAGAZINE, CALIBER, PRICE, BIO)
+VALUES (3, 'Walther P22 Junior', 'Semi-Automatic-Pistol', 10, '.22 LR', 9, 'Basic Gun');
+
+    INSERT INTO GUNS (ID, NAME, TYPE, MAGAZINE, CALIBER, PRICE,BIO)
+VALUES (4, 'Smith & Wesson 686', 'Revolver', 6, '.38 Special/.357 Magnum Magnum', 12, 'Basic Gun');
+
+    INSERT INTO GUNS(ID, NAME, TYPE, MAGAZINE, CALIBER, PRICE, BIO)
+VALUES (5, 'Taurus 95', 'Revolver', 8, '.22 LR', 10,'Basic Gun');
+
+    INSERT INTO GUNS (ID, NAME, TYPE, MAGAZINE, CALIBER, PRICE,BIO)
+VALUES (6, 'Ruger Super Redhawk', 'Revolver', 6, '44 Special/.44 Magnum Magnum', 12, 'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
+VALUES (7,'Kalashnikov Classic','Assault Rifle',35,'7.62x25 mm', 15,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
+VALUES (8,'Steyr Aug','Assault Rifle',30,'5.56x45 mm', 15,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
+VALUES (9,'Mosin Nagant','Bolt Action Rifle',5,'7.62x54 mm', 15,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
+VALUES (10,'AR-15','Semi-Automatic-Carbine',30,'5.56x45 mm', 12,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
+VALUES (11,'UZI','Submachinegun',25,'9x19 mm',18,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
+VALUES (12,'Mossberg 590A1','Pump Action Shotgun',8,'12 GA',15,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
+VALUES (13,'Saiga 12','Semi-Automatic-Shotgun',5,'12 GA',15,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
+VALUES (14,'M14','Semi-Automatic-Rifle',20,'7.62x51 mm',12,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
+VALUES (15,'M16A1 Vietnam','Assault Rifle',30,'5.56x45 mm',12,'Basic Gun');
+
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
+VALUES (16,'Colt M1911','Semi-Automatic-Pistol',7,'.45 ACP',10,'Basic Gun');
+
