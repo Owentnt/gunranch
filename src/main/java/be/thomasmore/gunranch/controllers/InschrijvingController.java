ed24098a7fcb1e966708ccfc9eff999a799356ee
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-03-01 16:52:49 +0100

    login knop toegevoegd

--- src/main/java/be/thomasmore/gunranch/controllers/InschrijvingController.java
+++ src/main/java/be/thomasmore/gunranch/controllers/InschrijvingController.java
@@ -1,9 +1,23 @@
 package be.thomasmore.gunranch.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InschrijvingController {
 
+    @GetMapping("/login")
+    public String login(Model model){
+        String firstName = "Owen";
+        String lastName = "Talboom";
+        String emailAdres = "owentm031111@gmail.com";
+        String phoneNumber = "0494 31 00 04";
+        model.addAttribute("firstName",firstName);
+        model.addAttribute("lastName",lastName);
+        model.addAttribute("emailAdres",emailAdres);
+        model.addAttribute("phoneNumber",phoneNumber);
+        model.addAttribute("model",model);
+        return "login";
+    }
 }
