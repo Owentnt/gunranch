c685c94383375c1fe060f33bf87e8f53608d66f5
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-03-04 20:08:07 +0100

    afbeeldingen toegevoegd in database part 2 Bio Taurus 95 stond dubbel in de database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,12 +33,9 @@ VALUES (5, 'Taurus 95', 'Revolver', 8, '.22 LR', 10, '/images/Taurus_95.png','Th
                                                                                      so it''s advisable to check for the latest information.');
 
     INSERT INTO GUNS (ID, NAME, TYPE, MAGAZINE, CALIBER, PRICE,IMAGE,BIO)
-VALUES (6, 'Ruger Super Redhawk', 'Revolver', 6, '44 Special/.44 Magnum Magnum', 12, '/images/Redhawk.png ','The Taurus Model 95 is a revolver designed and manufactured by the Brazilian firearms company Taurus International.
-                                                                                     It is chambered in .38 Special and .357 Magnum. Known for its compact size and reliability,
-                                                                                     the Taurus 95 is often chosen for concealed carry.
-                                                                                     It typically features a 5-round cylinder and is available in various barrel lengths.
-                                                                                     Keep in mind that firearm models may have undergone updates or changes,
-                                                                                     so it''s advisable to check for the latest information.');
+VALUES (6, 'Ruger Super Redhawk', 'Revolver', 6, '44 Special/.44 Magnum Magnum', 12, '/images/Redhawk.png ','The Ruger Super Redhawk is a line of double-action revolvers manufactured by Sturm, Ruger & Co.
+                                                                                                            The Super Redhawk was first introduced in 1987 as a powerful and rugged firearm designed for both hunting and personal defense.
+                                                                                                            It is known for its strength, durability, and versatility, making it a popular choice among handgun enthusiasts.');
 
     INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,IMAGE,BIO)
 VALUES (7,'Kalashnikov Classic','Assault Rifle',35,'7.62x25 mm', 15, '/images/kalashnikov.png','The Kalashnikov Classic refers to a series of firearms designed by Mikhail Kalashnikov,
@@ -51,58 +48,58 @@ VALUES (8,'Steyr Aug','Assault Rifle',30,'5.56x45 mm', 15, '/images/steyr_aug.pn
                                                           The AUG''s bullpup design allows for a shorter overall length without sacrificing barrel length, making it more maneuverable in tight spaces.
                                                           It has been adopted by various military and law enforcement agencies worldwide.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
-VALUES (9,'Mosin Nagant','Bolt Action Rifle',5,'7.62x54 mm', 15,'The Mosin-Nagant is a bolt-action rifle that originated in Russia
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,IMAGE,BIO)
+VALUES (9,'Mosin Nagant','Bolt Action Rifle',5,'7.62x54 mm', 15,'/images/mosin_nagant.png','The Mosin-Nagant is a bolt-action rifle that originated in Russia
                                                                 and became one of the most widely used military rifles in the 20th century.
                                                                 It was designed by Russian and Belgian inventors, Sergei Mosin and Léon Nagant.
                                                                 Introduced in the late 19th century, it played a significant role in both World War I and World War II.
                                                                 Known for its ruggedness and simplicity, the Mosin-Nagant has gained popularity among collectors and enthusiasts.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
-VALUES (10,'AR-15','Semi-Automatic-Carbine',30,'5.56x45 mm', 12,'The AR-15 is a lightweight, magazine-fed, gas-operated, and air-cooled rifle.
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE, IMAGE,BIO)
+VALUES (10,'AR-15','Semi-Automatic-Carbine',30,'5.56x45 mm', 12, '/images/AR15.png','The AR-15 is a lightweight, magazine-fed, gas-operated, and air-cooled rifle.
                                                                 Originally designed by Eugene Stoner in the 1950s, it became the basis for the M16 rifle used by the U.S. military.
                                                                 The AR-15 is known for its modular design and is widely used in civilian applications, often customized for various purposes.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
-VALUES (11,'UZI','Submachinegun',25,'9x19 mm',18,'The UZI is an Israeli submachine gun that gained global recognition.
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,IMAGE,BIO)
+VALUES (11,'UZI','Submachinegun',25,'9x19 mm',18,'/images/UZI.png','The UZI is an Israeli submachine gun that gained global recognition.
                                                  Designed in the 1940s by Uziel Gal, it''s known for its compact size and open-bolt, blowback-operated design.
                                                  The Uzi has been used by military and law enforcement worldwide, becoming an iconic firearm.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
-VALUES (12,'Mossberg 590A1','Pump Action Shotgun',8,'12 GA',15,'The Mossberg 590A1 is a pump-action shotgun known for its durability and reliability.
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,IMAGE,BIO)
+VALUES (12,'Mossberg 590A1','Pump Action Shotgun',8,'12 GA',15,'/images/mossberg590A1','The Mossberg 590A1 is a pump-action shotgun known for its durability and reliability.
                                                                It''s often used by military and law enforcement due to its robust construction and ability to handle heavy loads.
                                                                Features include a 20-inch barrel, 9-round capacity, and a reinforced aluminum receiver.
                                                                The 590A1 is designed to meet rigorous military standards and is popular for home defense and tactical use.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
-VALUES (13,'Saiga 12','Semi-Automatic-Shotgun',5,'12 GA',15,'The Saiga-12 is a Russian-made semi-automatic shotgun designed for both civilian and military use.
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE, IMAGE,BIO)
+VALUES (13,'Saiga 12','Semi-Automatic-Shotgun',5,'12 GA',15,'/images/Saiga12.png','The Saiga-12 is a Russian-made semi-automatic shotgun designed for both civilian and military use.
                                                             It''s based on the AK-47 platform and is known for its magazine-fed design, typically using detachable box magazines.
                                                             The Saiga-12 is popular for its versatility and adaptability, often used in shooting sports and by military and law enforcement agencies.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
-VALUES (14,'M14','Semi-Automatic-Rifle',20,'7.62x51 mm',12,'The M14 is a selective fire, magazine-fed battle rifle adopted by the United States military in the late 1950s.
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,IMAGE,BIO)
+VALUES (14,'M14','Semi-Automatic-Rifle',20,'7.62x51 mm',12,'/images/M14.png','The M14 is a selective fire, magazine-fed battle rifle adopted by the United States military in the late 1950s.
                                                            It served as the standard-issue rifle for the U.S. military until it was replaced by the M16 in the 1960s.
                                                            The M14 is known for its 7.62x51mm NATO caliber and distinctive design, featuring a wooden stock.
                                                            It has seen continued use in various roles and is appreciated for its accuracy and stopping power.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,BIO)
-VALUES (15,'M16A1 Vietnam','Assault Rifle',30,'5.56x45 mm',12,'The M16A1 was the standard issue rifle for U.S. troops during the Vietnam War.
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER,PRICE,IMAGE,BIO)
+VALUES (15,'M16A1 Vietnam','Assault Rifle',30,'5.56x45 mm',12,'/images/M16A1Vietnam.png','The M16A1 was the standard issue rifle for U.S. troops during the Vietnam War.
                                                               It replaced the M14 and was known for its lightweight design and selective fire capabilities.
                                                               The rifle had a 20-round magazine and a distinctive forward-assist feature to help chamber rounds.
                                                               However, early versions faced reliability issues due to inadequate cleaning kits and improper ammunition, which were later addressed.
                                                               The M16A1 played a significant role in the conflict and continued to serve in various capacities for decades.');
 
-    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
-VALUES (16,'Colt M1911','Semi-Automatic-Pistol',7,'.45 ACP',10,'The Colt M1911 is a renowned semi-automatic pistol designed by John Browning and adopted by the United States military in 1911.
+    INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,IMAGE,BIO)
+VALUES (16,'Colt M1911','Semi-Automatic-Pistol',7,'.45 ACP',10,'/images/colt1911.png','The Colt M1911 is a renowned semi-automatic pistol designed by John Browning and adopted by the United States military in 1911.
                                                                Known for its .45 ACP caliber and iconic design, it served as the standard-issue sidearm for the U.S. military until 1985.
                                                                The M1911 is celebrated for its reliability, stopping power, and has become a symbol of American military firearms history.');
 
-INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
-VALUES (17,'M1 Garand','Semi-Automatic-Rifle',8,'.30-06 Springfield Ridge',24,'The M1 Garand is a semi-automatic rifle that gained prominence during World War II and became the standard-issue rifle for the United States military.
+INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE, IMAGE,BIO)
+VALUES (17,'M1 Garand','Semi-Automatic-Rifle',8,'.30-06 Springfield Ridge',24, '/images/M1GARAND.png','The M1 Garand is a semi-automatic rifle that gained prominence during World War II and became the standard-issue rifle for the United States military.
                                                               Designed by Canadian-American firearms engineer John Garand, the M1 Garand is renowned for being the first semi-automatic rifle to be widely adopted by a major military power.');
 
-INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,BIO)
-VALUES (18,'Mauser Kar98K','Bolt-Action Rifle',5,'7,92x57mm',15,'The Mauser Karabiner 98 Kurz, commonly known as the Kar98k, is a bolt-action rifle that gained widespread recognition and use during World War II.');
+INSERT INTO GUNS(ID,NAME,TYPE,MAGAZINE,CALIBER, PRICE,IMAGE,BIO)
+VALUES (18,'Mauser Kar98K','Bolt-Action Rifle',5,'7,92x57mm',15,'/images/mauser.png','The Mauser Karabiner 98 Kurz, commonly known as the Kar98k, is a bolt-action rifle that gained widespread recognition and use during World War II.');
 
 
 
