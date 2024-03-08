bea4d60f3c41c8b7efb0a568be9039de6dce0eaf
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-03-08 22:34:20 +0100

    phoneNumber pattern test

--- src/main/java/be/thomasmore/gunranch/controllers/UserController.java
+++ src/main/java/be/thomasmore/gunranch/controllers/UserController.java
@@ -4,34 +4,33 @@ import be.thomasmore.gunranch.model.Reservation;
 import be.thomasmore.gunranch.repositorys.ReservationRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Validated
 public class UserController {
 
     @Autowired
     ReservationRepository reservationRepository;
 
     @GetMapping("/reservations")
-    public String reservations(Model model){
+    public String reservationsForm(Model model){
         model.addAttribute("model",model);
         model.addAttribute("reservations", new Reservation());
         return "reservations";
     }
 
     @PostMapping("/reservations")
-    public String submitReservation(@Valid Reservation reservations, BindingResult bindingResult, Model model){
+    public String submitReservationForm(@Valid Reservation reservations, BindingResult bindingResult, Model model){
         if (bindingResult.hasErrors()){
             return "reservations";
         }
         model.addAttribute("reservations",reservations);
-
-
         return "redirect:/reservationdetails";
     }
 
