e63346214e52f5feda016d4505cf6cf9ae35d8b1
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-03-08 23:37:33 +0100

    participationform en competitionsdetails toegevoegd

--- src/main/java/be/thomasmore/gunranch/repositorys/ParticipantRepository.java
+++ src/main/java/be/thomasmore/gunranch/repositorys/ParticipantRepository.java
@@ -1,4 +1,7 @@
 package be.thomasmore.gunranch.repositorys;
 
-public interface ParticipantRepository {
+import be.thomasmore.gunranch.model.Participants;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ParticipantRepository extends CrudRepository<Participants,Integer> {
 }
