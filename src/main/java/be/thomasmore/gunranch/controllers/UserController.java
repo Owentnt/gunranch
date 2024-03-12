665b839bfae9c6ef517cf0534c1c07dd70ed8165
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-03-12 12:47:31 +0100

    troubleshooting

--- src/main/java/be/thomasmore/gunranch/controllers/UserController.java
+++ src/main/java/be/thomasmore/gunranch/controllers/UserController.java
@@ -38,6 +38,6 @@ public class UserController {
     public String reservationDetais(Model model){
         Iterable<Reservation> reservations = reservationRepository.findAll();
         model.addAttribute("reservations",reservations);
-        return "/reservationdetails";
+        return "reservationdetails";
     }
 }
