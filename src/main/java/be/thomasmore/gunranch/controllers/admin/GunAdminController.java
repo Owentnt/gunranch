18093fac1c6e45cfe80ca70c057bd39c8b0d1999
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-03-15 16:38:00 +0100

    formulier gemaakt op editguns maar nog niet volledig klaar

--- src/main/java/be/thomasmore/gunranch/controllers/admin/GunAdminController.java
+++ src/main/java/be/thomasmore/gunranch/controllers/admin/GunAdminController.java
@@ -1,7 +1,10 @@
 package be.thomasmore.gunranch.controllers.admin;
 
+import be.thomasmore.gunranch.model.Guns;
+import be.thomasmore.gunranch.repositorys.GunsRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class GunAdminController {
 
+    @Autowired
+    private GunsRepository gunsRepository;
+
     private final Logger logger = LoggerFactory.getLogger(GunAdminController.class);
 
     @GetMapping({"/editguns/{id}","/editguns"})
-    public String editGuns(Model model, @PathVariable Integer id) {
+    public String editGuns(Model model, @PathVariable Integer id, Guns guns) {
+        model.addAttribute("id",id);
+        model.addAttribute("guns",guns);
         logger.info(String.format("gunEdit" + id ));
         return "admin/editguns";
     }
