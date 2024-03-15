bb742c0e1f87f32afc1f47521a37228f50fa7cc1
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-03-15 17:15:47 +0100

    findGun controller toegevoegd

--- src/main/java/be/thomasmore/gunranch/controllers/admin/GunAdminController.java
+++ src/main/java/be/thomasmore/gunranch/controllers/admin/GunAdminController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
@@ -26,6 +29,15 @@ public class GunAdminController {
         model.addAttribute("guns",guns);
         logger.info(String.format("gunEdit" + id ));
         return "admin/editguns";
+    }
+    @ModelAttribute("guns")
+    public Guns findGun(@PathVariable (required = false) Integer id){
+        logger.info("findGun "+id);
+        Optional<Guns> gunFinder = gunsRepository.findById(id);
+
+        if (gunFinder.isPresent())
+            return gunFinder.get();
+        return null;
     }
 
 }
