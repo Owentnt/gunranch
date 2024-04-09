cf7778a1abc8193c811d0c0a35935883fc112584
Owentnt <120107905+Owentnt@users.noreply.github.com>
2024-04-09 21:17:56 +0200

    styling

--- src/main/java/be/thomasmore/gunranch/repositories/UserRepository.java
+++ src/main/java/be/thomasmore/gunranch/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package be.thomasmore.gunranch.repositories;
 
 import be.thomasmore.gunranch.model.Users;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,5 +11,7 @@ public interface UserRepository extends CrudRepository<Users,Integer> {
 
    Optional<Users> findByUsername(String username);
 
+   @Query("SELECT SUM(gp.price) FROM Reservation b JOIN b.gunsPackage gp WHERE b.id = :bookingId")
+   Double calculateTotalPriceByBookingId(@Param("bookingId") Long bookingId);
 
 }
